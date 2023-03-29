5faa0f2b41b7ee3ccc9ae2c24930ea5e96ee9dbf
Juan Alfredo Salas Santillana <juan.salas.santillana@ucsp.edu.pe>
2023-03-29 04:18:49 -0500

    removed upload image docker

--- infra/index.ts
+++ infra/index.ts
@@ -17,7 +17,8 @@ const service = new awsx.ecs.FargateService("pichincha-api-" + stack, {
     taskDefinitionArgs: {
         containers: {
             pichinchaApp: {
-                image: awsx.ecs.Image.fromPath("pichincha-api-" + stack, "../"),
+                //image: awsx.ecs.Image.fromPath("pichincha-api-" + stack, "../"), Just if the runner have docker
+                image: '773686371801.dkr.ecr.us-east-1.amazonaws.com/pichincha-api-dev-b4d5e84:49d200300bae281001fa08a786dac48903d186fd3da6826460b891c5d39ba020',
                 memory: 512,
                 portMappings: [listener],
             },
